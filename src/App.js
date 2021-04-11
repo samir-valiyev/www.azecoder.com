a0e8cb1f73c732c2430c7d7b66e220a97e90e4fd
Samir Valiyev <samir.valiyev@gmail.com>
2021-04-11 14:26:12 +0400

    favicon.svg added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			<header>header</header>
+			<main>main</main>
+			<footer>footer</footer>
+		</div>
+	);
+}
+
+export default App;
