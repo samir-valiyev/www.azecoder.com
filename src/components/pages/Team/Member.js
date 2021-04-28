a3c3ae6eb321cd24796dd7c2a61e6a0c46b593b6
samirvaliyev <samir.valiyev@gmail.com>
2021-04-28 14:02:01 +0400

    team members section added

--- src/components/pages/Team/Member.js
+++ src/components/pages/Team/Member.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-function Member() {
+function Member(props) {
 	return (
-		<div>
-			<h1>ds</h1>
+		<div className="team__member">
+			<img className="team__member__img" src={props.img} alt="Samir Valiyev" />
+			<h2 className="team__member__name">{props.name}</h2>
+			<p className="team__member__profession">Front End Developer</p>
+
+			<div className="team__member__socials">
+				<a href={props.github} target="_blank" rel="noreferrer">
+					<i class="fab fa-github-square"></i>
+				</a>
+				<a href={props.linkedin} target="_blank" rel="noreferrer">
+					<i class="fab fa-linkedin"></i>
+				</a>
+
+				<a href={props.twitter} target="_blank" rel="noreferrer">
+					<i class="fab fa-twitter-square"></i>
+				</a>
+				<a href={props.facebook} target="_blank" rel="noreferrer">
+					<i class="fab fa-facebook-square"></i>
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/components/pages/Team/Team.js
+++ src/components/pages/Team/Team.js
@@ -1,7 +1,87 @@
 import React from "react";
+import Member from "./Member";
+import SamirValiyev from "../../../images/team/samirvaliyev.jpg";
 
 function Team() {
-	return <div></div>;
+	return (
+		<div className="team">
+			<h3>Team Members</h3>
+			<div className="team__members">
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+				<Member
+					img={SamirValiyev}
+					name={"Samir Valiyev"}
+					profession={"Front End Developer"}
+					github={"https://github.com/samirvaliyev"}
+					linkedin={"https://www.linkedin.com/in/samir-valiyev-a0b40832/"}
+					twitter={"https://twitter.com/samirvaliyev89"}
+					facebook={"https://www.facebook.com/samirvaliyev89/"}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Team;
